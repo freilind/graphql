1a00d30bdba3c32bb57a1d9f0603ffd3e4d5264a
Freilin Manzano <freilin.manzano@externos.bci.cl>
2020-12-01 18:31:46 -0300

    change mongo url by var env

--- Lyrical-GraphQL/server/server.js
+++ Lyrical-GraphQL/server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = 'mongodb+srv://dbadmin:1qazx@cluster0.dhskz.mongodb.net/lyricaldb?retryWrites=true&w=majority';
+const MONGO_URI = process.env.MONGO_URI;
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
